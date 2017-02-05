24565f60237b2a0a6b6719900acc0bd566b001e2
CYC666 <13705038428@163.com>
2017-02-05 11:57:22 +0800

    1、模拟器有一些分享平台出不来 2、有些平台出来了，但是对应的开发平台app ID不对应上，所以不能使用

---
+++ shareSDK_demo/shareSDK_demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  shareSDK_demo
+//
+//  Created by CaoYicheng on 2017/2/4.
+//  Copyright © 2017年 CYC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
